798c5ae482200d24afecea8deeb324e613c46032
Sravanthigangavara <163724314+Sravanthigangavara@users.noreply.github.com>
2025-09-23 00:32:05 +0530

    Update WebConfig.java

--- src/main/java/com/klef/config/WebConfig.java
+++ src/main/java/com/klef/config/WebConfig.java
@@ -1,15 +1,26 @@
 package com.klef.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // For serving static uploads
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/"); // from project-root/uploads
+    }
+
+    // For fixing CORS issue
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // all endpoints
+                .allowedOrigins("*") // allow all origins (for dev)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
